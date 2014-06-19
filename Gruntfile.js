a6e25b4f0d89e7a4e3cd93afbaed6b88a434534d
Juanjo <juanjo.lopez@biko2.com>
2014-06-19 18:06:58 +0200

    Detecto cambios en js para  regenerar jekyll tb

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function (grunt) {
       },
       jekyll: {
         files: [
-          '<%= yeoman.app %>/**/*.{html,yml,md,mkd,markdown}',
+          '<%= yeoman.app %>/**/*.{html,yml,md,mkd,markdown,js}',
           '!<%= yeoman.app %>/_bower_components/**/*'
         ],
         tasks: ['jekyll:server']
